cda78706a93c6d3e0ebf09be4a802ec3457783bf
preykriti <prakriteeshrestha2@gmail.com>
2025-06-11 07:33:12 +0000

    adding content through form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,21 @@
 class IdeasController < ApplicationController
 
     def index
+        # fetch all of the Idea objects in the database
+        # @ideas can be used in views
+        @ideas = Idea.all
+    end
 
+    def create 
+        # @idea = Idea.create(description: params[:idea][:description], author: params[:idea][:author])
+        @idea = Idea.create(idea_params)
+        redirect_to root_path
+    end
+
+    # this implements strong parameters which whitelists only certain attributes
+    private
+
+    def idea_params
+        params.require(:idea).permit(:description, :author)
     end
 end

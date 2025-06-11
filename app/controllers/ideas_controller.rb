40cb43800a1a029fc7def62a468ccc982ec2712a
preykriti <prakriteeshrestha2@gmail.com>
2025-06-11 08:50:00 +0000

    added edit and update function

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -12,6 +12,20 @@ class IdeasController < ApplicationController
         redirect_to root_path
     end
 
+    def edit
+        @idea = Idea.find(params[:id])
+    end
+
+    def update
+        @idea = Idea.find(params[:id])
+        if @idea.update(idea_params)
+            redirect_to root_path
+        else
+            redirect_to edit_idea_path(params[:id])
+        end
+    end
+        
+
     # this implements strong parameters which whitelists only certain attributes
     private
 
